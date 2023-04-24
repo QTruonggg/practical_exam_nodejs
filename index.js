2b73527a987a7728b685315df7163809d2c2cce8
QTruonggg <quangtruong102000@gmail.com>
2023-04-24 19:32:44 +0700

    ex

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+// connect mongodb
+require("dotenv").config();
+const database = require("./src/database");
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("server is running...");
+})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", function (req,res){
+    let student = {
+        name : "Quang Truong",
+        age: 24
+    };
+    let classRoom = {
+        name: "T2203E",
+        room: "B14"
+    }
+    res.render("home",{
+        student: student,
+        classRoom: classRoom
+    });
+});
+
